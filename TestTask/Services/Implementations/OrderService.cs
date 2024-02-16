76bf740ee8080301e95f3b5fe4a6ac9ebf7cffc9
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-02-16 16:47:51 +0300

    Test task done

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrderService(IServiceProvider serviceProvider)
+        {
+            _dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+        }
+
+        public Task<Order> GetOrder()
+        {
+            var mostValuableOrder = _dbContext.Orders
+                .OrderByDescending(o => o.Price * o.Quantity)
+                .FirstOrDefaultAsync();
+
+            return mostValuableOrder;
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            var orders = _dbContext.Orders.Where(o => o.Quantity > 10).ToListAsync();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserService(IServiceProvider serviceProvider)
+        {
+            _dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+        }
+
+        public Task<User> GetUser()
+        {
+            var topOrderUser = _dbContext.Users
+                .OrderByDescending(u => u.Orders.Count)
+                .FirstOrDefaultAsync();
+
+            return topOrderUser;
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            var inactiveUsers = _dbContext.Users.Where(u => u.Status == UserStatus.Inactive).ToListAsync();
+
+            return inactiveUsers;
+        }
+    }
+}
